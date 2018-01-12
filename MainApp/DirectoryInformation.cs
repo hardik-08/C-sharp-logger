43530ef7a26d6a5bd7249338bb5b3a70a4823b62
hardik-08 <hthallar8@gmail.com>
2018-01-12 21:41:47 +0530

    BAsic Directoy input

---
+++ MainApp/DirectoryInformation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+     class DirectoryInformation
+    {
+
+        private string path;
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+
+        public DirectoryInformation(string path)
+        {
+            this.Path = path;
+        }
+       
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,17 @@ namespace MainApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, please enter directoy you want to listen to, (absolute path)");
+            String directory = Console.ReadLine();
+            if (Directory.Exists(directory))
+            {
+                Console.WriteLine("It is a directory!");
+                DirectoryInformation directoryInfo = new DirectoryInformation(directory);
+                Console.WriteLine("You have chosen : "+directoryInfo.Path);
+            }
+            else
+                Console.WriteLine("Invalid Path, exiting");
+            Console.ReadLine();
         }
     }
 }
