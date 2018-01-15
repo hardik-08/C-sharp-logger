bc0b3e0a87913848994934a07ac2d40fa445c918
hardik-08 <hthallar8@gmail.com>
2018-01-15 20:50:53 +0530

    logging to file

---
+++ MainApp/DirectoryLogger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainApp
+{
+    class DirectoryLogger
+    {
+
+        private string path;
+        private FileSystemWatcher fsw;
+        public string LogPath { get; set; }
+
+        public string Path
+        {
+            get { return path; }
+            set { path = value; }
+        }
+
+
+        public DirectoryLogger(string path,string logPath)
+        {
+            this.Path = path;
+            this.LogPath = logPath;
+            setWatcher();
+        }
+
+        private void setWatcher()
+        {
+            fsw = new FileSystemWatcher();
+            fsw.Path = path;
+            fsw.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            fsw.Changed += new FileSystemEventHandler(printChange);
+            fsw.Renamed += new RenamedEventHandler(printChange);
+            fsw.Created += new FileSystemEventHandler(printChange);
+            fsw.Deleted += new FileSystemEventHandler(printChange);
+            fsw.IncludeSubdirectories = true;
+            fsw.EnableRaisingEvents = true;
+        }
+
+        void printChange(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("CHANGED, NAME: " + e.Name);
+            Console.WriteLine("CHANGED, FULLPATH: " + e.FullPath);
+            Console.WriteLine("WHAT HAPPENED? :" + e.ChangeType);
+            using (StreamWriter sw = File.AppendText(LogPath))
+            {
+                sw.Write("Logged at {0} : ", DateTime.Now.ToString("h:mm:ss tt"));
+                sw.Write(" CHANGED, NAME: " + e.Name);
+                sw.Write(" CHANGED, FULLPATH: " + e.FullPath);
+                sw.WriteLine(" WHAT HAPPENED? :" + e.ChangeType);
+            }
+
+        }
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -12,38 +12,21 @@ namespace MainApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, please enter directoy you want to listen to, (absolute path)");
-            String directory = Console.ReadLine();
-
+            string directory = Console.ReadLine();
             if (Directory.Exists(directory))
             {
                 Console.WriteLine("It is a directory!");
-                DirectoryInformation directoryInfo = new DirectoryInformation(directory);
+                string LogPath = Console.ReadLine();
+                DirectoryLogger directoryInfo = new DirectoryLogger(directory,LogPath);
                 Console.WriteLine("You have chosen : "+directoryInfo.Path+" We are now listening to it!");
-
-                FileSystemWatcher fsw = new FileSystemWatcher();
-                fsw.Path = directory;
-                fsw.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-                fsw.Changed += new FileSystemEventHandler(printChange);
-                fsw.Renamed += new RenamedEventHandler(printChange);
-                fsw.Created += new FileSystemEventHandler(printChange);
-                fsw.Deleted += new FileSystemEventHandler(printChange);
-                fsw.IncludeSubdirectories = true;
-                fsw.EnableRaisingEvents = true;
-
-
-
+                
             }
             else
                 Console.WriteLine("Invalid Path, exiting");
-            while (true) ; 
+            
             Console.ReadLine();
         }
 
-        static void printChange(object sender, FileSystemEventArgs e)
-        {
-            Console.WriteLine("CHANGED, NAME: " + e.Name);
-            Console.WriteLine("CHANGED, FULLPATH: " + e.FullPath);
-            Console.WriteLine("WHAT HAPPENED? :"+e.ChangeType);
-        }
+        
     }
 }
